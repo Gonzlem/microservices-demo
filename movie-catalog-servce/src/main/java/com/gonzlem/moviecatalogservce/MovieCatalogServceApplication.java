9deb76f94911e07093704f1f8d03e4bc4529533f
gonzlem <43175424@comunidadort.edu.ar>
2021-08-28 18:22:21 -0300

    Added RestTemplate singleton bean to not create multiple instances throughout the project.

--- movie-catalog-servce/src/main/java/com/gonzlem/moviecatalogservce/MovieCatalogServceApplication.java
+++ movie-catalog-servce/src/main/java/com/gonzlem/moviecatalogservce/MovieCatalogServceApplication.java
@@ -2,9 +2,15 @@ package com.gonzlem.moviecatalogservce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class MovieCatalogServceApplication {
+    @Bean
+    public RestTemplate getRestTemplate(){ // Provide a singleton for every class to be able to use it.
+        return new RestTemplate();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(MovieCatalogServceApplication.class, args);
--- movie-catalog-servce/src/main/java/com/gonzlem/moviecatalogservce/resources/MovieCatalogResource.java
+++ movie-catalog-servce/src/main/java/com/gonzlem/moviecatalogservce/resources/MovieCatalogResource.java
@@ -3,6 +3,7 @@ package com.gonzlem.moviecatalogservce.resources;
 import com.gonzlem.moviecatalogservce.models.CatalogItem;
 import com.gonzlem.moviecatalogservce.models.Movie;
 import com.gonzlem.moviecatalogservce.models.Rating;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,16 +18,20 @@ import java.util.stream.Collectors;
 @RequestMapping("/catalogs")
 public class MovieCatalogResource {
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     @GetMapping("/{userId}")
     private List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
-        RestTemplate restTemplate = new RestTemplate();
-        restTemplate.getForObject("http://localhost:8081/movies/foo", Movie.class);
+//        RestTemplate restTemplate = new RestTemplate(); // We no longer need this as we created a Bean to provide the singleton for us.
 
         List<Rating> ratings = Arrays.asList(
                 new Rating("PepaPig", 4),
                 new Rating("Terminator", 3));
 
-        return ratings.stream().map(rating -> new CatalogItem("Terminator", "Test", 4))
-                .collect(Collectors.toList());
+        return ratings.stream().map(rating -> {
+            Movie movie = restTemplate.getForObject("http://localhost:8081/movies/" + rating.getMovieId(), Movie.class);
+            return new CatalogItem(movie.getTitle(), movie.getDescription(), rating.getRating());
+        }).collect(Collectors.toList());
     }
 }
